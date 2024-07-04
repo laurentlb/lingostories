f03be1c611e792506706cf4f71dce25b7b88ddb3
Laurent Le Brun <laurentlb@gmail.com>
2024-07-04 22:17:26 +0200

    Use a US flag as the voice has a US English accent

--- js/stories.js
+++ js/stories.js
@@ -1,4 +1,4 @@
 export const stories = [
     { id: "intro", title: "Introduction", imageCount: 1 },
-    { id: "park", title: "At the Park", imageCount: 5 },
+    { id: "park", title: "A Day in the Park", imageCount: 5 },
 ];
