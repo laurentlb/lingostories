106c2e76ccec1aea08cc9a6399e5c5d8b1e207f1
Laurent Le Brun <laurentlb@gmail.com>
2025-01-27 23:59:46 +0100

    Change stories order - most recent first

--- js/stories.js
+++ js/stories.js
@@ -1,12 +1,12 @@
 export const allStories = [
+    { id: "grocery", title: "The Grocery List", imageCount: 5,
+        released: ["en", "fr", "nl", "sv", "de", "pl", "es"] },
+    { id: "park", title: "A Day in the Park", imageCount: 5,
+        released: true,
+    },
     { id: "intro", title: "Introduction", imageCount: 1,
         released: true,
      },
-    { id: "park", title: "A Day in the Park", imageCount: 5,
-        released: true,
-     },
-    { id: "grocery", title: "The Grocery List", imageCount: 5,
-        released: ["en", "fr", "nl", "sv", "de", "pl", "es"] },
     { id: "numbers", title: "A Game of Numbers", imageCount: 0,
         released: false },
 ];
